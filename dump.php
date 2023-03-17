8b8fe00726cdd0f310372bd6c7808f67ce0de7c6
Mikhail Yevchenko <m..@uo1.net>
2023-03-17 14:49:44 +0200

    Remove dupes

--- dump.php
+++ dump.php
@@ -15,34 +15,18 @@ $file = new FileHandler;
 
 $config = require __DIR__.'/config/config.php';
 
-$files = [];
 $ids = [];
 
 //header('Content-Type: text/tab-separated-values');
 header('Content-Type: text/plain');
 
-$dl_uri_prefix = ($_SERVER['HTTPS'] !== 'off' ? 'http' : 'https') . "://" . $_SERVER['HTTP_HOST'] . preg_replace('/[^\\/]+$/', '', $_SERVER['REQUEST_URI']) . $config['outputFolder'] . '/';
-
-foreach($file->listFiles() as $file) {
-    if (preg_match('/(?:ph)?[\\da-f]{4,}/', $file['name'], $match)) {
-        $uri = $dl_uri_prefix . $file['name'];
-        $files[$match[0]] = $uri;
-    }
-}
-
-// var_dump($files);
-
-$out = fopen('php://output', 'w');
+$records = [];
 
 $fh = fopen($config['logFolder'] . '/metadata', 'r');
 while(($l = fgets($fh)) !== false) {
     $record = json_decode(base64url_decode($l), true);
 
     foreach($record as $v) {
-        if (!isset($files[$v['video_id']])) {
-            continue;
-        }
-
         $tags = array_map(function($c) { return $c['tag_name']; }, $v['tags']);
         $categories = array_map(function($c) { return $c['category']; }, $v['categories']);
         $pornstars = array_map(function($c) { return $c['pornstar_name']; }, $v['pornstars']);
@@ -51,9 +35,9 @@ while(($l = fgets($fh)) !== false) {
             // 1. mp4 ссылка для скачивания
             $files[$v['video_id']],
             // 2. сколько обрезать сначала
-            '',
+            @$v['cutFrom'],
             // 3. сколько обрезать в конце
-            '',
+            @$v['cutTo'],
             // 4. название ролика
             $v['title'],
             // 5. категории
@@ -66,15 +50,35 @@ while(($l = fgets($fh)) !== false) {
             $v['userTitle']
         ];
 
-        fputcsv($out, $row, "\t");
-        // echo json_encode($v, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-        // die;
+        $records[$v['video_id']] = $row;
+    }
+}
+
+
+fclose($fh);
+
+$dl_uri_prefix = ($_SERVER['HTTPS'] !== 'off' ? 'http' : 'https') . "://" . $_SERVER['HTTP_HOST'] . preg_replace('/[^\\/]+$/', '', $_SERVER['REQUEST_URI']) . $config['outputFolder'] . '/';
+
+$out = fopen('php://output', 'w');
+
+foreach($file->listFiles() as $file) {
+    if (preg_match('/(?:ph)?[\\da-f]{4,}/', $file['name'], $match)) {
+        $video_id = $match[0];
+
+        $uri = $dl_uri_prefix . $file['name'];
+        $files[$video_id] = $uri;
+
+        if (isset($records[$video_id])) {
+            $row = $records[$video_id];
+
+            array_unshift($row, $uri);
+
+            fputcsv($out, $row, "\t");
+        }
     }
 }
 
 fclose($out);
-
-fclose($fh);
 
 function base64url_decode($base64url)
 {
