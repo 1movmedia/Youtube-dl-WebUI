506ceb15c782e507a51d2da7871f7db348bc8713
Mikhail Yevchenko <m..@uo1.net>
2023-03-19 21:01:22 +0200

    Disable authentication in dumps

--- dump.php
+++ dump.php
@@ -4,14 +4,6 @@ require_once 'class/Session.php';
 require_once 'class/FileHandler.php';
 require_once 'class/URLManager.php';
 
-$session = Session::getInstance();
-
-if(!$session->is_logged_in())
-{
-    header("Location: login.php");
-    exit;
-}
-
 $config = require __DIR__.'/config/config.php';
 
 $file = new FileHandler;
