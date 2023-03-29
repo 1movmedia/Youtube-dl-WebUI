f0ffef771ee1bfcad4ecfa3a47411bdcbca8006a
Mikhail Yevchenko <m..@uo1.net>
2023-03-29 11:02:59 +0300

    Download only beginning what the end should be cutted

--- class/Downloader.php
+++ class/Downloader.php
@@ -263,7 +263,7 @@ class Downloader
 		if ($from != 0 || $from_end != 0) {
 			$cut_duration = $to - $from;
 
-			// $cmd .= " --download-sections " . escapeshellarg("*0-$to");
+			$cmd .= " --download-sections " . escapeshellarg("*0-$to");
 			// $cmd .= " --postprocessor-args " . escapeshellarg("-ss $from -t $cut_duration -avoid_negative_ts make_zero -map 0:0 -c:0 copy -map 0:1 -c:1 copy -map_metadata 0 -movflags +faststart -default_mode infer_no_subs -ignore_unknown");
 
 			$output_file = $this->download_path."/".$this->id . '.mp4';
