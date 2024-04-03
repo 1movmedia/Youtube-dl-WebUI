8ae01baf58362a453c28308a00a2362c5e46a9fc
Mikhail Yevchenko <m..@uo1.net>
2024-04-03 10:49:41 +0300

    Format

--- login.php
+++ login.php
@@ -1,23 +1,23 @@
 <?php
-	require_once 'class/Session.php';
-	require_once 'class/Downloader.php';
 
-	$session = Session::getInstance();
-	$loginError = "";
+require_once 'class/Session.php';
+require_once 'class/Downloader.php';
 
-	if(isset($_POST["username"]) && isset($_POST["password"]))
+$session = Session::getInstance();
+$loginError = "";
+
+if(isset($_POST["username"]) && isset($_POST["password"]))
+{
+	if($session->login($_POST["username"], $_POST["password"]))
 	{
-		if($session->login($_POST["username"], $_POST["password"]))
-		{
-			header("Location: index.php");
-			exit;
-		}
-		else
-		{
-			$loginError = "Wrong username or password !";
-		}
+		header("Location: index.php");
+		exit;
 	}
+	else
+	{
+		$loginError = "Wrong username or password !";
+	}
+}
 
-	require 'views/layout.php';
-	require 'views/login.php';
-
+require 'views/layout.php';
+require 'views/login.php';
