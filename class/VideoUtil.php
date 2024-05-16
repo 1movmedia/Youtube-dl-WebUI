2a1fa6281f8d34654270c6872d4de21c6a0f536a
Mikhail Yevchenko <m..@uo1.net>
2024-05-16 11:26:54 +0300

    Apply value contraints

--- class/VideoUtil.php
+++ class/VideoUtil.php
@@ -19,7 +19,7 @@ class VideoUtil {
             try {
                 $frames = [];
             
-                $ss = $after_position;
+                $ss = max(0, $after_position);
         
                 $successful = false;
                 
