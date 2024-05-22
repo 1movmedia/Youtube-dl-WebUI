19439f3650b82e938c84062048d89d63848c0dbd
Mikhail Yevchenko <m..@uo1.net>
2024-05-22 10:14:10 +0300

    Implement training set generator

--- test/generate_training_set.php
+++ test/generate_training_set.php
@@ -1,9 +1,15 @@
 <?php
 
+if (!empty($_SERVER['SERVER_NAME'])) {
+    die("Forbidden\n");
+}
+
+require __DIR__ . '/../www/vendor/autoload.php';
+
 $videos_file = __DIR__ . '/train_videos.json';
 
 if (file_exists($videos_file)) {
-    $videos = json_decode(file_get_contents($videos_file));
+    $videos = json_decode(file_get_contents($videos_file), true);
 }
 else {
     require_once __DIR__ . '/create_test_set.php';
@@ -13,6 +19,35 @@ else {
     file_put_contents($videos_file, json_encode($videos, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES));
 }
 
+$cache = __DIR__ . '/../tmp/cache';
+
+if (!is_dir($cache)) {
+    mkdir($cache, 0777, true);
+}
+
+$training_set_dir = __DIR__ . '/../taining_set';
+$training_set_dirs = [];
+foreach(['ok', 'promo'] as $class) {
+    $dir = $training_set_dir . '/' . $class;
+    $training_set_dirs[$class] = $dir;
+
+    if (!is_dir($dir)) {
+        mkdir($dir, 0777, true);
+    }
+}
+
 foreach($videos as $video_info) {
-    // TODO : cut frames and send to classfier. Store bad results into 2 sets.
+    $frame_classes = VideoAdFinder::classifyFrames($video_info['filename'], 0, $video_info['cutFrom'] * 2, $cache, true);
+
+    foreach($frame_classes as $class) {
+        $real_ad = $class['timestamp'] < $video_info['cutFrom'];
+
+        if ($real_ad === $class['is_ad']) {
+            continue; // Correct result
+        }
+
+        $set_dir = $training_set_dirs[$real_ad ? 'promo' : 'ok'];
+
+        copy($class['filename'], $set_dir . '/' . $video_info['video_id'] . '-' . $class['timestamp'] . '.jpg');
+    }
 }
